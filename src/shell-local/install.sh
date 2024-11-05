713b24ac21c9aa27fc61ff305cccf65ec5c13c10
Michał Bachowski <michalbachowski@gmail.com>
2024-11-05 08:52:23 +0000

    Add the 'shell-local' feature

---
+++ src/shell-local/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+. ./library_scripts.bash
+
+FEATURE_NAME=shell-local
+
+if [ -n "$BASH_SCRIPT_PATH" ]; then
+    add_feature_shell_file "$FEATURE_NAME (for bash_script_path)" "$BASH_SCRIPT_PATH" "bash"
+fi
+
+if [ -n "$ZSH_SCRIPT_PATH" ]; then
+    add_feature_shell_file "$FEATURE_NAME (for zsh_script_path)" "$ZSH_SCRIPT_PATH" "zsh"
+fi
---
+++ test/shell-local/_common_tests.sh
@@ -0,0 +1,22 @@
+source test_scripts.sh
+
+COMMON_DIR=/devcontainer-feature/michalbachowski
+
+BASHRC_FILE_PATH="${COMMON_DIR}/bashrc"
+BASH_SCRIPT_PATH=script.bash
+
+ZSHRC_FILE_PATH="${COMMON_DIR}/zshrc"
+ZSH_SCRIPT_PATH=script.zsh
+
+# $USER defaults to `root`
+if [ "$HAS_BASH" = "1" ]; then
+    test_shell_features "$BASH_SCRIPT_PATH" "bash" "$USER" "0"
+else
+    check "The [$BASH_SCRIPT_PATH] either DOES NOT exist or DOES NOT contain a reference to the [$BASH_SCRIPT_PATH] file" bash -c "test ! -f $BASHRC_FILE_PATH && exit 0; sudo cat '$BASHRC_FILE_PATH' | grep '$BASH_SCRIPT_PATH' | grep -q source"
+fi
+
+if [ "$HAS_ZSH" = "1" ]; then
+    test_shell_features "$ZSH_SCRIPT_PATH" "zsh" "$USER" "0"
+else
+    check "The [$ZSH_SCRIPT_PATH] either DOES NOT exist or DOES NOT contain a reference to the [$ZSH_SCRIPT_PATH] file" bash -c "test ! -f $ZSHRC_FILE_PATH && exit 0; sudo cat '$ZSHRC_FILE_PATH' | grep '$ZSH_SCRIPT_PATH' | grep -q source"
+fi
