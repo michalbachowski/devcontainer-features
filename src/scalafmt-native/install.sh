2ca192285f5f5766ecb3db65563aaef95f3bda53
Michał Bachowski <michalbachowski@gmail.com>
2024-11-05 13:06:37 +0000

    Install curl if missing

--- src/scalafmt-native/install.sh
+++ src/scalafmt-native/install.sh
@@ -5,6 +5,28 @@ URL=https://raw.githubusercontent.com/scalameta/scalafmt/v${VERSION}/bin/install
 INSTALL_LOCATION=/usr/local/bin/scalafmt-native
 INSTALLER_LOCATION=/tmp/install-scalafmt-native.sh
 
+if command -v scalafmt-native; then
+    echo "The [scalafmt-native] binary exists, skipping installation."
+fi
+
+if ! command -v curl; then
+    . ./library_scripts.bash
+
+    # nanolayer is a cli utility which keeps container layers as small as possible
+    # source code: https://github.com/devcontainers-contrib/nanolayer
+    # `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
+    # and if missing - will download a temporary copy that automatically get deleted at the end 
+    # of the script
+    ensure_nanolayer nanolayer_location "v0.5.6"
+
+    # install curl
+    $nanolayer_location \
+        install \
+        devcontainer-feature \
+        "ghcr.io/rocker-org/devcontainer-features/apt-packages:1" \
+        --option packages='curl' --option version="$VERSION"
+fi
+
 status=$(curl \
     --fail \
     --location \
@@ -14,7 +36,7 @@ status=$(curl \
     $URL)
 
 if [ "$status" -ne 200 ] || [ ! -f $INSTALLER_LOCATION ]; then
-    echo "The snyk binary could not be downloaded from [$URL; HTTP status: $status], please fix it!"
+    echo "The [scalafmt-native] binary could not be downloaded from [$URL; HTTP status: $status], please fix it!"
     exit 1
 fi
 
--- src/snyk-cli/install.sh
+++ src/snyk-cli/install.sh
@@ -10,6 +10,24 @@ if [ -f $DEST ]; then
     exit 0
 fi
 
+if ! command -v curl; then
+    . ./library_scripts.sh
+
+    # nanolayer is a cli utility which keeps container layers as small as possible
+    # source code: https://github.com/devcontainers-contrib/nanolayer
+    # `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
+    # and if missing - will download a temporary copy that automatically get deleted at the end 
+    # of the script
+    ensure_nanolayer nanolayer_location "v0.5.6"
+
+    # install curl
+    $nanolayer_location \
+        install \
+        devcontainer-feature \
+        "ghcr.io/rocker-org/devcontainer-features/apt-packages:1" \
+        --option packages='curl' --option version="$VERSION"
+fi
+
 sysarch=$(uname -m)
 
 case $sysarch in \
