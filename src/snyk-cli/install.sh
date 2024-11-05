d921f60da1f540b9c2646ecefba633de3d7ce215
Michał Bachowski <michalbachowski@gmail.com>
2024-11-05 17:03:32 +0000

    Fail snyk-cli feature when curl is missing

--- src/snyk-cli/install.sh
+++ src/snyk-cli/install.sh
@@ -11,21 +11,13 @@ if [ -f $DEST ]; then
 fi
 
 if ! command -v curl; then
-    . ./library_scripts.sh
-
-    # nanolayer is a cli utility which keeps container layers as small as possible
-    # source code: https://github.com/devcontainers-contrib/nanolayer
-    # `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
-    # and if missing - will download a temporary copy that automatically get deleted at the end 
-    # of the script
-    ensure_nanolayer nanolayer_location "v0.5.6"
-
-    # install curl
-    $nanolayer_location \
-        install \
-        devcontainer-feature \
-        "ghcr.io/rocker-org/devcontainer-features/apt-packages:1" \
-        --option packages='curl' --option version="$VERSION"
+    echo ""
+    echo "========================"
+    echo "Command [curl] not found. Please install it first!"
+    echo ""
+    echo "========================"
+    echo ""
+    exit 1
 fi
 
 sysarch=$(uname -m)
