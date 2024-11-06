1081715eb4ba6cecd3dc8380019ff36db50fe6bc
Michał Bachowski <michalbachowski@gmail.com>
2024-11-06 08:00:16 +0000

    Do not install curl when missing.

--- src/scalafmt-native/install.sh
+++ src/scalafmt-native/install.sh
@@ -12,21 +12,13 @@ if -f $INSTALL_LOCATION; then
 fi
 
 if ! command -v curl >/dev/null; then
-    . ./library_scripts.sh
-
-    # nanolayer is a cli utility which keeps container layers as small as possible
-    # source code: https://github.com/devcontainers-contrib/nanolayer
-    # `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
-    # and if missing - will download a temporary copy that automatically get deleted at the end 
-    # of the script
-    ensure_nanolayer nanolayer_location "v0.5.6"
-
-    # install curl
-    $nanolayer_location \
-        install \
-        devcontainer-feature \
-        "ghcr.io/rocker-org/devcontainer-features/apt-packages:1" \
-        --option packages='curl' --option version="$VERSION"
+    echo ""
+    echo "========================"
+    echo "Command [curl] not found. Please install it first!"
+    echo ""
+    echo "========================"
+    echo ""
+    exit 1
 fi
 
 status=$(curl \
