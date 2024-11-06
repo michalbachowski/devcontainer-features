32535baea02432b22252e8ffd257004df7a460ca
Michał Bachowski <michalbachowski@gmail.com>
2024-11-06 11:43:39 +0000

    Sync library_scripts.sh

--- src/shell-local/library_scripts.sh
+++ src/shell-local/library_scripts.sh
@@ -1,11 +1,48 @@
+export DCFMB_COMMON_DIR=/devcontainer-feature/michalbachowski
+export ENV_FILE_PATH=/etc/environment
+
+function ensure_common_dir
+{
+    # create common dir
+    mkdir -p $DCFMB_COMMON_DIR
+    chown -R root $DCFMB_COMMON_DIR
+    chmod -R 0555 $DCFMB_COMMON_DIR
+
+}
+
+function copy_feature_files
+{
+    local feature_name="$1"
+    local feature_files_source_path="${2:-$(pwd)}"
+    local env_name_suffix="$(echo $feature_name | tr '[:lower:]' '[:upper:]' | tr '-' '_')"
+
+    local feature_files_dest_path="${DCFMB_COMMON_DIR}/$1"
+
+    ensure_common_dir
+
+    if [ -d "$feature_files_dest_path" ]; then
+        echo "The feature [$feature_name] directory has been cached already, skipping"
+        return
+    fi
+
+    echo "For the [$feature_name] feature copying source [$feature_files_source_path] to the [$feature_files_dest_path] feature cache path"
+    cp -r "$feature_files_source_path" "$feature_files_dest_path"
+    chown root "$feature_files_dest_path"
+    chmod 0555 "$feature_files_dest_path"
+
+    set_env "DCFMB_${env_name_suffix}_PATH" "$feature_files_path"
+
+    echo "Final state of the [$feature_name] feature cache dir is:"
+    ls -la "$feature_files_dest_path"
+}
 
 function add_feature_shell_file
 {
     set -e
 
-    feature_name="$1"
-    custom_script_path="$2"
-    user_shell="$3"
+    local feature_name="$1"
+    local custom_script_path="$2"
+    local user_shell="$3"
 
     echo "Activating feature [$feature_name]"
 
@@ -18,11 +55,12 @@ function add_feature_shell_file
         exit 1
     fi
 
-    COMMON_DIR=/devcontainer-feature/michalbachowski
-    DEST_SCRIPT_PATH="$COMMON_DIR/$custom_script_path"
-    DEST_SHELLRC_PATH="$COMMON_DIR/${user_shell}rc"
+    ensure_common_dir
 
-    user_shellrc_path="$_REMOTE_USER_HOME/.${user_shell}rc"
+    local DEST_SCRIPT_PATH="$DCFMB_COMMON_DIR/$custom_script_path"
+    local DEST_SHELLRC_PATH="$DCFMB_COMMON_DIR/${user_shell}rc"
+
+    local user_shellrc_path="$_REMOTE_USER_HOME/.${user_shell}rc"
 
     if [ ! -d "$_REMOTE_USER_HOME" ]; then
         echo "The [$_REMOTE_USER_HOME] home directory for the [$_REMOTE_USER] user not found, aborting!"
@@ -33,11 +71,6 @@ function add_feature_shell_file
         echo "The [$DEST_SCRIPT_PATH] file exists, skipping"
         exit 0
     fi
-
-    # create common dir
-    mkdir -p $COMMON_DIR
-    chown -R root $COMMON_DIR
-    chmod -R 0555 $COMMON_DIR
 
     if [ -z "$custom_script_path" ]; then
         echo "The custom script path is empty, aborting!"
@@ -75,4 +108,17 @@ function add_feature_shell_file
     fi
 
     echo 'Done!'
+}
+
+function set_env
+{
+    env_name="$1"
+    value="${2:-$CERT_PATH}"
+
+    if ! cat $ENV_FILE_PATH | grep $env_name | grep "$value"; then
+        echo "Setting environment variable [$env_name] to [$value] in [$ENV_FILE_PATH]"
+        echo "${env_name}=\"$value\"" >> $ENV_FILE_PATH
+    else
+        echo "The [$ENV_FILE_NAME] contains already the [$env_name] variable with the [$value] value";
+    fi
 }
