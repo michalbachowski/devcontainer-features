00aa74bf6e28195aa2218a6573bb60d459713376
Michał Bachowski <michalbachowski@gmail.com>
2024-11-06 12:59:03 +0000

    Fix tests

--- test/bash-sensible/_common_tests.sh
+++ test/bash-sensible/_common_tests.sh
@@ -1,9 +1,10 @@
 source test_scripts.sh
 
-SENSIBLE_FILE_NAME=sensible.bash
+FEATURE_NAME=bash-sensible
+SENSIBLE_FILE_NAME="$COMMON_DIR/$FEATURE_NAME/sensible.bash"
 USER_SHELL=bash
 
 # $USER defaults to `root`
-test_shell_features bash-sensible "$SENSIBLE_FILE_NAME" "$USER_SHELL"
+test_shell_features $FEATURE_NAME "$SENSIBLE_FILE_NAME" "$USER_SHELL"
 
 check "When sourcing the [$user_rc_file_path] script file, the prompt takes effect" $USER_SHELL -i -c "test \"\$HISTIGNORE\" = '&:[ ]*:exit:ls:bg:fg:history:clear' || exit 1"
--- test/zsh-prompt-currenttime/_common_tests.sh
+++ test/zsh-prompt-currenttime/_common_tests.sh
@@ -1,8 +1,9 @@
 source test_scripts.sh
 
-PROMPT_FILE_NAME=prompt-currenttime.zsh
+FEATURE_NAME=zsh-prompt-currenttime
+PROMPT_FILE_NAME="$COMMON_DIR/$FEATURE_NAME/prompt-currenttime.zsh"
 USER_SHELL=zsh
 
-test_shell_features zsh-prompt-currenttime "$PROMPT_FILE_NAME" "$USER_SHELL"
+test_shell_features $FEATURE_NAME "$PROMPT_FILE_NAME" "$USER_SHELL"
 
 check "When sourcing the [$user_rc_file_path] script file, the prompt takes effect" $USER_SHELL -i -c "echo \$RPS1 | grep -q reset_color"
