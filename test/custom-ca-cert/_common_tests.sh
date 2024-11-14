0bf87e7d41e3ca892d4ea7348e481c6e90aeb3cc
Michał Bachowski <michalbachowski@gmail.com>
2024-11-14 15:13:05 +0000

    Sync test_scripts.sh

--- test/custom-ca-cert/_common_tests.sh
+++ test/custom-ca-cert/_common_tests.sh
@@ -4,29 +4,25 @@ source test_scripts.sh
 export ENV_FILE_PATH="/etc/environment"
 PIP_CONF_PATH=/etc/pip.conf
 NPM_CONFIG_PATH=/etc/npmrc
+CERT_PATH="${CERT_PATH:-/optcert.pem}"
 
-function check_env_is_set
+function check_env_is_set_
 {
     env_name="$1"
     path="${2:-$ENV_FILE_PATH}"
-    cert="${3:-${CERT_PATH:-/optcert.pem}}"
-    check "The [$env_name] prop is set in the [$path] and contains [$cert] as a value" bash -c "cat $path | grep $env_name | grep -q '$cert'"
 
-    # for environment variables, check effective values
-    if [ "$path" = "$ENV_FILE_PATH" ]; then
-        check "The [$env_name] prop is effectively set to [$cert]" bash -i -c "set -a; source /etc/environment; [ \"\$$env_name\" = \"$cert\" ] || exit 1"
-    fi
+    check_env_is_set "$env_name" "$CERT_PATH" "$path"
 }
 
 check "test" bash -c "cat /etc/environment"
 
-check_env_is_set CURL_CA_BUNDLE
-check_env_is_set NODE_EXTRA_CA_CERTS
-check_env_is_set NODE_CONFIG_CAFILE
-check_env_is_set PIP_CERT
-check_env_is_set REQUESTS_CA_BUNDLE
+check_env_is_set_ CURL_CA_BUNDLE
+check_env_is_set_ NODE_EXTRA_CA_CERTS
+check_env_is_set_ NODE_CONFIG_CAFILE
+check_env_is_set_ PIP_CERT
+check_env_is_set_ REQUESTS_CA_BUNDLE
 # got "both" test case effective cert is different!
-check_env_is_set SSL_CERT_FILE 
+check_env_is_set_ SSL_CERT_FILE 
 
-check_env_is_set cert $PIP_CONF_PATH
-check_env_is_set cafile $NPM_CONFIG_PATH
+check_env_is_set_ cert $PIP_CONF_PATH
+check_env_is_set_ cafile $NPM_CONFIG_PATH
--- test/custom-ca-cert/test_scripts.sh
+++ test/custom-ca-cert/test_scripts.sh
@@ -6,34 +6,47 @@ source dev-container-features-test-lib
 set -e
 
 export COMMON_DIR=/devcontainer-feature/michalbachowski
+export ENV_FILE_PATH="/etc/environment"
+
+function get_user
+{
+    if [ "$NON_ROOT_USER" = "1" ]; then
+        echo testuser
+    else
+        echo root
+    fi
+}
+
+function get_homedir
+{
+    if [ "$NON_ROOT_USER" = "1" ]; then
+        echo /home/$(get_user)
+    else
+        echo /root
+    fi
+}
 
 function test_shell_features
 {
-    feature_name="$1"
-    script_file_path="$2"
-    user_shell="$3"
-    script_file_must_exists="${4:-1}"
+    local feature_name="$1"
+    local script_file_path="$2"
+    local user_shell="$3"
+    local script_file_must_exists="${4:-1}"
 
-
-    if [ "$NON_ROOT_USER" = "1" ]; then
-        USER=testuser
-        HOMEDIR=/home/$USER
-    else
-        USER=root
-        HOMEDIR=/root
-    fi
+    local user="$(get_user)"
+    local homedir="$(get_homedir)"
 
     FEATURE_CACHE_DIR=${COMMON_DIR}/$feature_name
 
     rc_file_name="${user_shell}rc"
     export rc_file_path=$COMMON_DIR/$rc_file_name
-    export user_rc_file_path=$HOMEDIR/.$rc_file_name
+    export user_rc_file_path=$homedir/.$rc_file_name
 
     # Feature-specific tests
     # The 'check' command comes from the dev-container-features-test-lib. Syntax is...
     # check <LABEL> <cmd> [args...]
 
-    check "Logging" bash -c "ls -la ${script_file_path} ${rc_file_path} ${user_rc_file_path}; echo -n $USER' : [UID]='$UID'; [id cmd]='; id -u $USER;  exit 0"
+    check "Logging" bash -c "ls -la ${script_file_path} ${rc_file_path} ${user_rc_file_path}; echo -n $user' : [UID]='$UID'; [id cmd]='; id -u $user;  exit 0"
     if [ "$script_file_must_exists" = "1" ]; then
         check "The [$script_file_path] file exists" $user_shell -c "test -f $script_file_path"
         check "The [$script_file_path] file is owned by the [root] user" $user_shell -c "test \"$(stat -c '%u' $script_file_path)\" = \"$(id -u root)\" || exit 1"
@@ -41,9 +54,9 @@ function test_shell_features
     check "The $rc_file_name [$rc_file_path] file exists" $user_shell -c "test -f $rc_file_path"
     check "The $rc_file_name [$rc_file_path] file exists is owned by the [root] user" $user_shell -c "test \"$(stat -c '%u' $rc_file_path)\" = \"$(id -u root)\" || exit 1"
     check "The $rc_file_name [$rc_file_path] file contains a reference to the [$script_file_path] file" $user_shell -c "cat $rc_file_path | grep '$script_file_path' | grep source"
-    check "The user's [$USER] $rc_file_name [$user_rc_file_path] file exists" $user_shell -c "test -f $user_rc_file_path"
-    check "The user's [$USER] $rc_file_name [$user_rc_file_path] file exists is owned by the [$USER] user" $user_shell -c "test \"\$(whoami)\" = \"$USER\" && test -O $user_rc_file_path || exit 1"
-    check "The user's [$USER] $rc_file_name [$user_rc_file_path] file contains a reference to the feature's $rc_file_name [$rc_file_path] file" $user_shell -c "cat '$user_rc_file_path' | grep '$rc_file_path' | grep source"
+    check "The user's [$user] $rc_file_name [$user_rc_file_path] file exists" $user_shell -c "test -f $user_rc_file_path"
+    check "The user's [$user] $rc_file_name [$user_rc_file_path] file exists is owned by the [$user] user" $user_shell -c "test \"\$(whoami)\" = \"$user\" && test -O $user_rc_file_path || exit 1"
+    check "The user's [$user] $rc_file_name [$user_rc_file_path] file contains a reference to the feature's $rc_file_name [$rc_file_path] file" $user_shell -c "cat '$user_rc_file_path' | grep '$rc_file_path' | grep source"
 }
 
 function test_binary_feature
@@ -57,4 +70,17 @@ function test_binary_feature
     check "binary is owned by root:root" bash -c "ls -l $script_path | grep  'root root'"
     check "binary is available on PATH" bash -c "command -v $script_name >/dev/null || exit 1"
     check "binary is in correct version" bash -c "$script_name --version | grep $script_version"
+}
+
+function check_env_is_set
+{
+    env_name="$1"
+    value="${2}"
+    path="${3:-$ENV_FILE_PATH}"
+    check "The [$env_name] prop is set in the [$path] and contains [$value] as a value" bash -c "cat $path | grep $env_name | grep -q '$value'"
+
+    # for environment variables, check effective values
+    if [ "$path" = "$ENV_FILE_PATH" ]; then
+        check "The [$env_name] prop is effectively set to [$value]" bash -i -c "set -a; source /etc/environment; [ \"\$$env_name\" = \"$value\" ] || exit 1"
+    fi
 }
