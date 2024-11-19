e243ae3fac71296e12fc03041616a7ceaba8b5d4
Michał Bachowski <michalbachowski@gmail.com>
2024-11-19 13:35:51 +0000

    Fix scenario-based tests for shell-execute-custom-script

--- test/shell-execute-custom-script/_common_tests.sh
+++ test/shell-execute-custom-script/_common_tests.sh
@@ -15,30 +15,37 @@ ZSHRC_FILE_PATH="${COMMON_DIR}/zshrc"
 ZSH_SCRIPT_PATH=$FEATURE_DIR/zsh_scripts
 
 # $USER defaults to `root`
+
 if [ "$HAS_BASH" = "1" ] || [ "$HAS_COMMON" = "1" ]; then
     check Debugging bash -c "cat $BASH_SCRIPT_PATH"
     test_shell_features $FEATURE_NAME "$BASH_SCRIPT_PATH" "bash"
-    check "The [$BASH_SCRIPT_PATH] contains the [$BASH_SCRIPT] script" bash -c "cat $BASH_SCRIPT_PATH | grep -F '$BASH_SCRIPT'"
 
     BASH_USER_RC_FILE_PATH=$user_rc_file_path
-else
-    check "The [$BASHRC_FILE_PATH] either DOES NOT exist or DOES NOT contain a reference to the [$BASH_SCRIPT_PATH] file" bash -c "test ! -f $BASHRC_FILE_PATH && exit 0; cat '$BASHRC_FILE_PATH' | grep '$BASH_SCRIPT_PATH' | grep -q source && exit 1"
 fi
 
 if [ "$HAS_ZSH" = "1" ] || [ "$HAS_COMMON" = "1" ]; then
     check Debugging bash -c "cat $ZSH_SCRIPT_PATH"
     test_shell_features $FEATURE_NAME "$ZSH_SCRIPT_PATH" "zsh"
-    check "The [$ZSH_SCRIPT_PATH] contains the [$ZSH_SCRIPT] script" bash -c "cat $ZSH_SCRIPT_PATH | grep -F '$ZSH_SCRIPT'"
 
     ZSH_USER_RC_FILE_PATH=$user_rc_file_path
-else
-    check "The [$ZSHRC_FILE_PATH] either DOES NOT exist or DOES NOT contain a reference to the [$ZSH_SCRIPT_PATH] file" bash -c "test ! -f $ZSHRC_FILE_PATH && exit 0; cat '$ZSHRC_FILE_PATH' | grep '$ZSH_SCRIPT_PATH' | grep -q source && exit 1"
+fi
+
+if [ "$HAS_BASH" = "1" ]; then
+    check "The [$BASH_SCRIPT_PATH] contains the [$BASH_SCRIPT] script" bash -c "cat $BASH_SCRIPT_PATH | grep -F '$BASH_SCRIPT'"
+elif [ "$HAS_COMMON" = "0" ]; then
+    check "The [$BASHRC_FILE_PATH] either DOES NOT exist or DOES NOT contain a reference to the [$BASH_SCRIPT_PATH] file" bash -c "test ! -f $BASHRC_FILE_PATH && exit 0; cat '$BASHRC_FILE_PATH' | grep '$BASH_SCRIPT_PATH' | grep source && exit 1"
+fi
+
+if [ "$HAS_ZSH" = "1" ]; then
+    check "The [$ZSH_SCRIPT_PATH] contains the [$ZSH_SCRIPT] script" bash -c "cat $ZSH_SCRIPT_PATH | grep -F '$ZSH_SCRIPT'"
+elif [ "$HAS_COMMON" = "0" ]; then
+    check "The [$ZSHRC_FILE_PATH] either DOES NOT exist or DOES NOT contain a reference to the [$ZSH_SCRIPT_PATH] file" bash -c "test ! -f $ZSHRC_FILE_PATH && exit 0; cat '$ZSHRC_FILE_PATH' | grep '$ZSH_SCRIPT_PATH' | grep source && exit 1"
 fi
 
 if [ "$HAS_COMMON" = "1" ]; then
     check "The [$BASH_SCRIPT_PATH] contains the [$COMMON_SCRIPT] script" bash -c "cat $BASH_SCRIPT_PATH | grep -F \"$COMMON_SCRIPT\""
     check "The [$ZSH_SCRIPT_PATH] contains the [$COMMON_SCRIPT] script" bash -c "cat $ZSH_SCRIPT_PATH | grep -F \"$COMMON_SCRIPT\""
 else
-    check "The [$BASH_SCRIPT_PATH] DOES NOT contain the [$COMMON_SCRIPT] script" bash -c "cat $BASH_SCRIPT_PATH | grep -F \"$COMMON_SCRIPT\" && exit 1"
-    check "The [$ZSH_SCRIPT_PATH] DOES NOT contain the [$COMMON_SCRIPT] script" bash -c "cat $ZSH_SCRIPT_PATH | grep -F \"$COMMON_SCRIPT\" && exit 1"
+    check "The [$BASH_SCRIPT_PATH] either DOES NOT exist or DOES NOT contain the [$COMMON_SCRIPT] script" bash -c "test ! -f $BASH_SCRIPT_PATH && exit 0; cat $BASH_SCRIPT_PATH | grep -F \"$COMMON_SCRIPT\" && exit 1 || exit 0"
+    check "The [$ZSH_SCRIPT_PATH] either DOES NOT exist or DOES NOT contain the [$COMMON_SCRIPT] script" bash -c "test ! -f $ZSH_SCRIPT_PATH && exit 0; cat $ZSH_SCRIPT_PATH | grep -F \"$COMMON_SCRIPT\" && exit 1 || exit 0"
 fi
