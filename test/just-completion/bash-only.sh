76c79f9656b43ab50ca8bfc77c3d996d168f6e2a
Michał Bachowski <michalbachowski@gmail.com>
2025-01-09 13:33:21 +0000

    Add the just-completion feature

---
+++ test/just-completion/bash-only.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+NO_ZSH="1"
+source _common_tests.sh
+
+reportResults
---
+++ test/just-completion/non-root-user.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# the container user does not change the installation and completion files are still owner by the `root` user
+source _common_tests.sh
+
+reportResults
