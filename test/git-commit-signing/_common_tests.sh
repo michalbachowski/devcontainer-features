badc82d36e76a4fa127bb1f91cf8f49d6ca459a3
Michał Bachowski <michalbachowski@gmail.com>
2025-04-18 08:31:10 +0000

    Setup Git signing on the system level

--- test/git-commit-signing/_common_tests.sh
+++ test/git-commit-signing/_common_tests.sh
@@ -2,25 +2,13 @@ source test_scripts.sh
 
 FEATURE_NAME=git-commit-signing
 
-FEATURE_DIR="${COMMON_DIR}/${FEATURE_NAME}"
-SETUP_FILE_PATH="${FEATURE_DIR}/git-setup.sh"
-
 function check_config
 {
     local opt="${1}"
     local val="${2}"
 
-    check "The [$opt] prop with [$val] value is set in the [$SETUP_FILE_PATH] file" bash -c "cat $SETUP_FILE_PATH | grep -F '$opt' | grep -q -F '$val'"
-
-    if [ "$CHECK_GIT" != "0" ]; then
-        check "The [$opt] prop with [$val] value is set for current git repository" bash -c "test \$(git config '$opt') = '$val'"
-    fi
+    check "The [$opt] prop with [$val] value is set for current git repository" bash -c "test \$(git config --system '$opt') = '$val'"
 }
-
-check "DEBUG" bash -c "cat '${SETUP_FILE_PATH}'"
-
-check "The [${FEATURE_DIR}] directory exists" bash -c "test -d '${FEATURE_DIR}'"
-check "The [${SETUP_FILE_PATH}] file exists" bash -c "test -f '${SETUP_FILE_PATH}'"
 
 check_config "gpg.format" "${FORMAT:-ssh}"
 check_config "commit.gpgsign" "true"
