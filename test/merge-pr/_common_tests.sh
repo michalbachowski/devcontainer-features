7b8d9106cdc5fee2f13d3364fc0c1f9e5c76c11e
Michał Bachowski <michalbachowski@gmail.com>
2026-03-30 15:23:42 +0000

    Update env variables for [merge-pr]
    Mimic values for rust-related env variables
    from the [ghcr.io/devcontainers/features/rust] feature
    for consistency

--- test/merge-pr/_common_tests.sh
+++ test/merge-pr/_common_tests.sh
@@ -1,6 +1,6 @@
 source test_scripts.sh
 
-BIN_DIR="${COMMON_DIR}/cargo-home/bin"
+BIN_DIR="/usr/local/cargo/bin"
 BIN_PATH="${BIN_DIR}/merge-pr"
 # Empty version, since merge-pr does not expose version information.
 # If the version is missing, the installation will fail anyway.
